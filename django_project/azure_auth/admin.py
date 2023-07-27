7cbac9a8137e653c5f7a168dcd8621c208afe51c
Danang <danangmassandy@gmail.com>
2023-07-27 20:42:28 +0000

    add third party login

--- django_project/azure_auth/admin.py
+++ django_project/azure_auth/admin.py
@@ -1,7 +1,8 @@
 """Azure auth admin."""
 from django.contrib import admin
-
-from azure_auth.models import RegisteredDomain
+from django.urls import reverse
+from django.contrib.sites.models import Site
+from azure_auth.models import RegisteredDomain, ThirdPartyApplication
 
 
 class RegisteredDomainAdmin(admin.ModelAdmin):
@@ -11,4 +12,16 @@ class RegisteredDomainAdmin(admin.ModelAdmin):
     list_editable = ('group',)
 
 
+class ThirdPartyApplicationAdmin(admin.ModelAdmin):
+    """ThirdPartyApplication admin."""
+
+    list_display = ('name', 'origin', 'client_id', 'auth_url')
+
+    def auth_url(self, obj: ThirdPartyApplication):
+        current_site = Site.objects.get_current()
+        scheme = 'https://'
+        url = reverse('azure_auth:third-party')
+        return f'{scheme}{current_site.domain}{url}?client_id={obj.client_id}'
+
 admin.site.register(RegisteredDomain, RegisteredDomainAdmin)
+admin.site.register(ThirdPartyApplication, ThirdPartyApplicationAdmin)
--- django_project/azure_auth/urls.py
+++ django_project/azure_auth/urls.py
@@ -4,6 +4,7 @@ from azure_auth.views import (
     azure_auth_login,
     azure_auth_logout,
     azure_auth_redirect,
+    azure_auth_third_party
 )
 
 from django.urls import path
@@ -13,8 +14,10 @@ app_name = "azure_auth"
 urlpatterns = [
     path("azure-auth/login", azure_auth_login, name="login"),
     path("azure-auth/logout", azure_auth_logout, name="logout"),
-    path("signin-oidc", azure_auth_callback, name="callback"),
+    # path("signin-oidc", azure_auth_callback, name="callback"),
+    path("auth/redirect", azure_auth_callback, name="callback"),
     path("redirect", azure_auth_redirect, name="redirect"),
+    path("azure-auth/third-party", azure_auth_third_party, name="third-party"),
 ]
 
 
