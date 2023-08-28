c97ddb3fd3a375ee80db3a7e6ad72eaae0a9b0e2
Danang <danangmassandy@gmail.com>
2023-08-28 12:09:47 +0700

    fix move directory azure

--- django_project/georepo/api_views/tile.py
+++ django_project/georepo/api_views/tile.py
@@ -1,40 +1,16 @@
 import os
+import io
 from django.conf import settings
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from azure.storage.blob import BlobServiceClient
 from azure.core.exceptions import ResourceNotFoundError
-from tempfile import SpooledTemporaryFile
+from georepo.utils.azure_blob_storage import StorageContainerClient
 
 
 class TileAPIView(APIView):
     permission_classes = [AllowAny]
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._service_client = None
-        self._client = None
-
-    def _get_service_client(self):
-        return BlobServiceClient.from_connection_string(
-            settings.AZURE_STORAGE
-        )
-
-    @property
-    def service_client(self):
-        if self._service_client is None:
-            self._service_client = self._get_service_client()
-        return self._service_client
-
-    @property
-    def client(self):
-        if self._client is None:
-            self._client = self.service_client.get_container_client(
-                settings.AZURE_STORAGE_CONTAINER
-            )
-        return self._client
 
     def build_response(self, file, y):
         response = HttpResponse(
@@ -55,12 +31,15 @@ class TileAPIView(APIView):
         if settings.USE_AZURE:
             source = f'layer_tiles/{resource_uuid}/{z}/{x}/{y}'
             try:
-                with SpooledTemporaryFile() as tmp_file:
-                    bc = self.client.get_blob_client(blob=source)
-                    download_stream = bc.download_blob()
-                    download_stream.readinto(tmp_file)
-                    tmp_file.seek(0)
-                    return self.build_response(tmp_file, y)
+                bc = StorageContainerClient.get_blob_client(blob=source)
+                download_stream = bc.download_blob(
+                    max_concurrency=2,
+                    validate_content=False
+                )
+                stream = io.BytesIO()
+                download_stream.readinto(stream)
+                stream.seek(0)
+                return self.build_response(stream, y)
             except ResourceNotFoundError:  # noqa
                 pass
         else:
