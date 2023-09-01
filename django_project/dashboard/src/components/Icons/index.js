b02bbfa49c76e3b73cf6d6b6a73eea9e1d152fa5
Zakki <age.zakki@gmail.com>
2023-09-01 11:08:47 +0700

    Remove unused icons

--- django_project/dashboard/src/components/Icons/index.js
+++
@@ -1,4 +0,0 @@
-// export { default as ArrowForwardIcon } from "./arrow-forward.svg";
-// export { default as ChatBubbleIcon } from "./chat-bubble.svg";
-// export { default as CloseIcon } from "./close.svg";
-// export { default as EmailIcon } from "./email.svg";
