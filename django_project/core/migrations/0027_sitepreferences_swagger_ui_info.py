261262666de0af35ffcca46117edb6559afcf112
Zakki <age.zakki@gmail.com>
2023-09-08 13:29:30 +0700

    Change order migration number

--- django_project/core/migrations/0027_sitepreferences_swagger_ui_info.py
+++ django_project/core/migrations/0027_sitepreferences_swagger_ui_info.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0025_delete_old_keys'),
+        ('core', '0026_update_tiling_configs'),
     ]
 
     operations = [
