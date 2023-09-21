c33dc046809ba0da6a461b64201d8c936afb3c1a
Zakki <age.zakki@gmail.com>
2023-09-21 14:12:17 +0700

    fix lint

--- django_project/dashboard/tools/find_country_max_level.py
+++ django_project/dashboard/tools/find_country_max_level.py
@@ -1,5 +1,4 @@
 import logging
-import time
 
 from dashboard.models.layer_upload_session import (
     LayerUploadSession
@@ -25,7 +24,6 @@ def find_country_max_level(
     From each country/entity upload at upload_session,
     find maximum level of layer file that has entity at that level
     """
-    start = time.time()
     available_levels = (
         upload_session.layerfile_set.values_list(
             'level', flat=True
