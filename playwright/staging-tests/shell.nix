286799dc1e52d7561a2475b6faaa6585947579eb
Tim Sutton <tim@kartoza.com>
2023-10-29 00:28:04 +0100

    Fix shel for nixos

--- playwright/staging-tests/shell.nix
+++ playwright/staging-tests/shell.nix
@@ -1,16 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+#let
+#  nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/9665f56e3b9c0bfaa07ca88b03b62bb277678a23.tar.gz";
+#  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
+#in
 let
-  nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/9665f56e3b9c0bfaa07ca88b03b62bb277678a23.tar.gz";
-  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
-in
-with pkgs;
-mkShell {
-  buildInputs = [
+  env_vars = ''
+    export FOO="bar"
+  '';
+  extraOutputsToInstall = ["man" "dev"];
+  multiPkgs = pkgs: with pkgs; [ zlib ];
+in pkgs.mkShell {
+  name = "env";
+  buildInputs = with pkgs; [
     nodejs
+    playwright
+    playwright-driver
     playwright-test
-    python311Packages.playwright
-    python311Packages.pytest
   ];
-
   PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}";
 
   shellHook = ''
