f3df217c1c72b004cf3acbe8be0de18c3c6b4fac
Tim Sutton <tim@kartoza.com>
2023-10-29 10:14:26 +0000

    Fixed nixos direnv by pinning to todays unstable tree of nixpkgs

--- playwright/staging-tests/shell.nix
+++ playwright/staging-tests/shell.nix
@@ -1,26 +1,25 @@
 { pkgs ? import <nixpkgs> {} }:
-#let
-#  nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/9665f56e3b9c0bfaa07ca88b03b62bb277678a23.tar.gz";
-#  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
-#in
+
 let
-  env_vars = ''
-    export FOO="bar"
-  '';
-  extraOutputsToInstall = ["man" "dev"];
-  multiPkgs = pkgs: with pkgs; [ zlib ];
+  rev = "63678e9f3d3afecfeafa0acead6239cdb447574c";
+  channel = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  config = {
+    allowBroken = false;
+  };
+  pkgs = import channel { inherit config; };
+
 in pkgs.mkShell {
-  name = "env";
   buildInputs = with pkgs; [
     nodejs
     playwright
     playwright-driver
     playwright-test
+    pkgs.playwright
   ];
   PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}";
-
   shellHook = ''
     # Remove playwright from node_modules, so it will be taken from playwright-test
     rm node_modules/@playwright/ -R
   '';
 }
+
