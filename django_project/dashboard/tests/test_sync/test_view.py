4f8c7cf79e0ba245263ceafe4a52bce0d70a5a15
Danang <danangmassandy@gmail.com>
2023-10-30 13:37:26 +0700

    fix test view sync status

--- django_project/dashboard/tests/test_sync/test_view.py
+++ django_project/dashboard/tests/test_sync/test_view.py
@@ -4,20 +4,42 @@ __copyright__ = ('Copyright 2023, Unicef')
 
 import urllib.parse
 
+import json
+from django.contrib.gis.geos import GEOSGeometry
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIRequestFactory
-
+from georepo.models.dataset_view import DatasetViewResource
 from dashboard.api_views.view_sync import ViewSyncList
 from georepo.tests.model_factories import (
-    UserF, DatasetF, DatasetViewF, ModuleF
+    UserF, DatasetF, DatasetViewF, ModuleF,
+    GeographicalEntityF,
+    EntityTypeF,
 )
 from georepo.utils.permission import (
     grant_dataset_manager
 )
+from core.settings.utils import absolute_path
+from georepo.utils.dataset_view import (
+    create_sql_view,
+    init_view_privacy_level,
+    calculate_entity_count_in_view
+)
 
 
 class TestViewSyncList(TestCase):
+
+    def set_view_as_synced(self, view):
+        create_sql_view(view)
+        init_view_privacy_level(view)
+        # init view entity count
+        calculate_entity_count_in_view(view)
+        resources = DatasetViewResource.objects.filter(
+            dataset_view=view,
+            entity_count__gt=0
+        )
+        for resource in resources:
+            resource.set_synced(vector_tiles=True, product=True)
 
     def setUp(self) -> None:
         self.factory = APIRequestFactory()
@@ -28,12 +50,35 @@ class TestViewSyncList(TestCase):
             module=self.module,
             generate_adm0_default_views=True
         )
+        self.entity_type = EntityTypeF.create(label='Country')
+        geojson_0_1_path = absolute_path(
+            'dashboard', 'tests',
+            'geojson_dataset', 'level_0_1.geojson')
+        with open(geojson_0_1_path) as geojson:
+            data = json.load(geojson)
+        geom_str = json.dumps(data['features'][0]['geometry'])
+        self.geographical_entity = GeographicalEntityF.create(
+            dataset=self.dataset,
+            type=self.entity_type,
+            is_validated=True,
+            is_approved=True,
+            is_latest=True,
+            geometry=GEOSGeometry(geom_str),
+            internal_code='PAK',
+            revision_number=1
+        )
         self.superuser = UserF.create(is_superuser=True)
         self.creator = UserF.create()
         self.dataset_view_1 = DatasetViewF.create(
             dataset=self.dataset,
-            created_by=self.creator
+            created_by=self.creator,
+            query_string=(
+                f'select * from georepo_geographicalentity where '
+                f'dataset_id={self.dataset.id}'
+            )
         )
+        self.set_view_as_synced(self.dataset_view_1)
+        self.dataset_view_1.refresh_from_db()
         grant_dataset_manager(self.dataset_view_1.dataset, self.creator)
 
     def test_list_views(self):
@@ -73,7 +118,7 @@ class TestViewSyncList(TestCase):
             'simplification_status': 'out_of_sync',
             'simplification_progress': 0.0,
             'vector_tiles_progress': 0.0,
-            'product_progress': 0.0,
+            'product_progress': 100.0,
             'permissions': ['Manage', 'Read']
         }
         self.assertEqual(
