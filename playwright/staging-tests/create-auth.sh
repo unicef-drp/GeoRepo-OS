6ff2b19236665bdc017bca7a167a76daacfd87fe
Tim Sutton <tim@kartoza.com>
2023-11-05 07:36:44 +0000

    Update tests so we can run the staging tests.

--- playwright/staging-tests/create-auth.sh
+++ playwright/staging-tests/create-auth.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 
-echo "This script will write a new test to tests/deleteme.ts"
+echo "This script will write a new test to tests/deleteme.spec.ts"
 echo "then delete it, leaving only the auth config."
 echo ""
 echo "When the playwright browser opens, log in to the site then exit."
 echo "After recording your test, close the test browser."
-echo "Recording auth token to georepo-auth.json"
+echo "Recording auth token to auth.json"
 
 # File exists and write permission granted to user
 # show prompt
 echo "Continue? y/n"
 read ANSWER
 case $ANSWER in 
-  [yY] ) echo "Writing georepo-auth.json" ;;
+  [yY] ) echo "auth.json" ;;
   [nN] ) echo "Cancelled."; exit ;;
 esac
 
-npx playwright \
-  codegen \
-  --save-storage=georepo-auth.json \
-  -o tests/deleteme.ts \
-  https://staging-georepo.unitst.org
+source base-url.sh
 
-# We are only interested in georepo-auth.json
-rm tests/deleteme.ts
+playwright \
+  codegen \
+  --target playwright-test \
+  --save-storage=auth.json \
+  -o tests/deleteme.spec.ts \
+  $BASE_URL
+
+# We are only interested in geosight-auth.json
+rm tests/deleteme.spec.ts
 
 echo "Auth file creation completed."
+echo "You can then run your tests by doing e.g.:"
+echo "playwright test --project chromium"
