a8fe67ebd1c5403cd65737ed071ee1649a022ab3
Danang Massandy <danangmassandy@gmail.com>
2025-04-12 06:22:04 +0000

    add signals to delete output zip file in blob storage

--- django_project/dashboard/models/blob_export.py
+++ django_project/dashboard/models/blob_export.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 from georepo.models import BaseTaskRequest
+from georepo.utils.azure_blob_storage import StorageContainerClient
 
 
 class BlobExportRequest(BaseTaskRequest):
@@ -27,3 +30,17 @@ class BlobExportRequest(BaseTaskRequest):
     def download(self, directory):
         """Download files in Blob storage path to directory."""
         pass
+
+
+@receiver(post_delete, sender=BlobExportRequest)
+def post_delete_export_request(sender, instance, **kwargs):
+    """Delete output file in blob storage."""
+    if instance.output_path:
+        try:
+            bc = StorageContainerClient.get_blob_client(
+                blob=instance.output_path
+            )
+            bc.delete_blob()
+        except Exception as e:
+            # Handle exception if needed
+            print(f'Error deleting blob: {e}')
