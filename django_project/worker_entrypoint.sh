1fc04466cd3fdc2ecadffd0f82fa9e4db1e63a4a
Danang <danangmassandy@gmail.com>
2025-09-19 21:21:35 +0100

    use supervisor to manage workers (#264)

--- django_project/worker_entrypoint.sh
+++ django_project/worker_entrypoint.sh
@@ -24,15 +24,8 @@ update-rc.d flower defaults
 sleep 2
 /etc/init.d/flower start
 
-TILE_C=${VECTOR_TILE_QUEUE_CONCURRENCY:-1}
-VALIDATE_C=${VALIDATE_QUEUE_CONCURRENCY:-3}
-EXPORTER_C=${EXPORT_DATA_QUEUE_CONCURRENCY:-1}
-
-# start tile and validate workers
-celery -A core multi start tile validate exporter -c:tile $TILE_C -c:validate $VALIDATE_C -c:exporter $EXPORTER_C -Q:tile tegola -Q:validate validation -Q:exporter exporter -l INFO --logfile=/tmp/workers.log
-
-# start default worker
-celery -A core worker -l INFO --logfile=/tmp/master_worker.log
+# start worker via supervisord
+/usr/bin/supervisord -c /supervisord.conf
 
 echo "-----------------------------------------------------"
 echo "FINISHED WORKER COMMAND --------------------------"
