aa6ef35f2bd97e7c661fffa9ab6d5f62a8906a39
Danang <danangmassandy@gmail.com>
2025-10-28 08:18:41 +0000

    Feat api dataset extended (#280)
    * add api search entity by admin level 0
    
    * add API to search parent and children in dataset
    
    * fix lint
    
    * add dataset fields to find entity by ucode API

--- django_project/georepo/urls_v1.py
+++ django_project/georepo/urls_v1.py
@@ -53,7 +53,10 @@ from georepo.api_views.entity import (
     EntityListByAdminLevelAndUCode,
     FindEntityById,
     FindEntityVersionsByConceptUCode,
-    FindEntityVersionsByUCode
+    FindEntityVersionsByUCode,
+    EntityListByAdminLevel0,
+    EntityTraverseHierarchyByUCode,
+    EntityTraverseChildrenHierarchyByUCode
 )
 module_urls = [
     path(
@@ -96,6 +99,10 @@ entity_urls = [
         r'(?P<admin_level>[\d]+)/?$',
         EntityListByAdminLevel.as_view(),
         name='search-entity-by-level'),
+    re_path(
+        r'search/dataset/(?P<uuid>[\da-f-]+)/entity/list/?$',
+        EntityListByAdminLevel0.as_view(),
+        name='search-entity-by-level-0'),
     path(
         'search/dataset/<uuid:uuid>/entity/level/'
         '<int:admin_level>/<path:ucode>/',
@@ -127,6 +134,16 @@ entity_urls = [
         EntityFuzzySearch.as_view(),
         name='entity-fuzzy-search-by-name'
     ),
+    path(
+        'search/dataset/<uuid:uuid>/entity/'
+        '<path:ucode>/parent/',
+        EntityTraverseHierarchyByUCode.as_view(),
+        name='search-entity-parent-by-ucode'),
+    path(
+        'search/dataset/<uuid:uuid>/entity/'
+        '<path:ucode>/children/',
+        EntityTraverseChildrenHierarchyByUCode.as_view(),
+        name='search-entity-children-by-ucode'),
 ]
 
 operation_entity_urls = [
@@ -319,15 +336,15 @@ controlled_list_urls = [
 urlpatterns = []
 urlpatterns += module_urls
 urlpatterns += dataset_urls
-# add dataset search by admin level
-urlpatterns += [
-    re_path(
-        r'search/dataset/(?P<uuid>[\da-f-]+)/entity/level/'
-        r'(?P<admin_level>[\d]+)/?$',
-        EntityListByAdminLevel.as_view(),
-        name='search-entity-by-level'
-    )
-]
+# # add dataset search by admin level
+# urlpatterns += [
+#     re_path(
+#         r'search/dataset/(?P<uuid>[\da-f-]+)/entity/level/'
+#         r'(?P<admin_level>[\d]+)/?$',
+#         EntityListByAdminLevel.as_view(),
+#         name='search-entity-by-level'
+#     )
+# ]
 urlpatterns += entity_urls
 urlpatterns += operation_entity_urls
 urlpatterns += view_entity_urls
